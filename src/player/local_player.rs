de131777885a025b917c877bbe84e08b6db98a28
Nicolas Brüggemann (mcbernie) <nb@zweiund40.com>
2022-06-07 13:55:10 +0200

    set fixed host to 127.0.0.1

--- src/player/local_player.rs
+++ src/player/local_player.rs
@@ -82,6 +82,7 @@ impl LocalPlayer {
        
         let tcp_client = make_element("tcpclientsrc", Some("local_tcpclient"))?;
         tcp_client.set_property("port", &port)?;
+        tcp_client.set_property("host", "127.0.0.1")?;
 
         let srcpad = tcp_client.static_pad("src").unwrap();
         let bus_clone = bus.clone();
