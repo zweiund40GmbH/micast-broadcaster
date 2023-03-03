4eb7d96cb527103496f76816d7155744e685f456
Nicolas Brüggemann (mcbernie) <nb@zweiund40.com>
2023-03-03 13:11:20 +0100

    push to the channel

--- src/broadcast/informip.rs
+++ src/broadcast/informip.rs
@@ -52,7 +52,7 @@ pub fn inform_clients() {
                 }
             }
 
-            thread::sleep(Duration::from_secs(1));
+            thread::sleep(Duration::from_secs(5));
 
         }
     });
@@ -63,7 +63,7 @@ pub fn dedect_server_ip() -> Receiver<IpAddr> {
 
     let (sender, receiver) = std::sync::mpsc::channel();
 
-    thread::spawn(|| {
+    thread::spawn(move || {
 
         loop {
             let mut receiver_sockets = Vec::new();
@@ -100,6 +100,7 @@ pub fn dedect_server_ip() -> Receiver<IpAddr> {
                     Ok((size, addr)) => {
                         let data = std::str::from_utf8(&buffer[..size]).unwrap();
                         info!("received datagramm from {} with {}", addr, data);
+                        let _ = sender.send(addr.clone().ip());
                         break;
                     },
                     _ => {}
